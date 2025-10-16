2334b50e5197c1ee1b0b07af93da0653cf8d7169
Aditya20097 <raj.aditya10997@gmail.com>
2025-10-16 09:57:03 +0530

    working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import { Route,Routes } from 'react-router'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
+import HomePage from './pages/HomePage'
+import OnBoarding from './pages/OnBoarding'
+import NotificationPage from './pages/NotificationPage'
+import ChatPage from './pages/ChatPage'
+import CallPage from './pages/CallPage'
 const App = () => {
   return (
     <div className=' h-screen' data-theme="night">
-      <h1>hello</h1>
+     <Routes>
+      <Route path='/' element ={< HomePage/>}/>
+        <Route path='/signup' element ={<SignUpPage/>}/>
+        <Route path='/login' element ={<LoginPage/>}/>
+        <Route path='/onboarding' element ={<OnBoarding/>}/>
+        <Route path='/notification' element ={<NotificationPage/>}/>
+        <Route path='/chat' element ={<ChatPage/>}/>
+        <Route path='/call' element ={<CallPage/>}/>
+
+        
+        
+     </Routes>
    </div>
   )
 }
