f2c1714d6eef7b846376e752dbbd8ace4f223d89
Aditya20097 <raj.aditya10997@gmail.com>
2025-10-17 11:45:18 +0530

    signup ui complete

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,9 +1,124 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {MessagesSquare, ShipWheelIcon} from 'lucide-react'
+import {Link} from 'react-router'
 
 const SignUpPage = () => {
+  const [signupData , setSignupData] = useState({
+    fullName:"",
+    email:"",
+    password:""
+  })
   
+  const handleSignup = (e) => {
+    e.preventDefault()
+  }
+
+
   return (
-    <div>SignUpPage</div>
+    <div className='h-screen flex items-center justify-center p-4 sm:p-6 md:p-8' data-theme = "forest">
+      <div className='border border-primary/25  flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden'>
+      {/* signup form left side */}
+      <div className='w-full lg:w-1/2 p-4 sm:p-8 flex flex-col'>
+      {/* logo */}
+      <div className='mb-4 flex items-center justify-start gap-2'>
+        <MessagesSquare className = 'size-9 text-primary'/>
+        <span className='text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider'>Talk-Flow</span>
+      </div>
+
+          <div className='w-full'>
+            <form onSubmit={handleSignup} >
+              <div className='space-y-4'>
+                <div>
+                  <h2 className='text-xl font-semibold'> Create an Account</h2>
+                  <p className='text-sm opacity-70'> Join Talk-Flow  and start your connections journey</p>
+                </div>
+
+                <div className='space-y-3'>
+                  {/* fULL nAME */}
+                  <div className='form-control w-full'>
+                    <label className='label'>
+                      <span className='label-text'> Full Name </span>
+                    </label>
+                    <input type="text" 
+                    placeholder='sorya sonu'
+                    className='input input-bordered w-full'
+                    value={signupData.fullName}
+                    onChange={(e) => setSignupData({...signupData,fullName:e.target.value})}
+                    required
+                    />
+                  </div>
+
+                  {/* EMAIL  */}
+
+                   <div className='form-control w-full'>
+                    <label className='label'>
+                      <span className='label-text'> Email </span>
+                    </label>
+                    <input type="email" 
+                    placeholder='soryasonu@1033.com'
+                    className='input input-bordered w-full'
+                    value={signupData.email}
+                    onChange={(e) => setSignupData({...signupData,email:e.target.value})}
+                    required
+                    />
+                  </div>
+                   <div className='form-control w-full'>
+                    <label className='label'>
+                      <span className='label-text'> Password </span>
+                    </label>
+                    <input type="password" 
+                    placeholder='**********'
+                    className='input input-bordered w-full'
+                    value={signupData.password}
+                    onChange={(e) => setSignupData({...signupData,password:e.target.value})}
+                    required
+                    />
+                    <p className='text-xs opacity-70 mt-1'> Password must be atleast 6 character</p>
+                  </div>
+                  {/* tC */}
+                  <div className='form-control' >
+                    <label className='label cursor-pointer justify-start gap-2'>
+                      <input type="checkbox" className='checkbox checkbox-sm' required />
+                      <span className='text-xs leading-tight'>
+                        I agree to the {" "}
+                        <span className='text-primary hover:underline'> Terms of service</span> and {" "}
+                        <span className='text-primary hover:underline'>Privacy Policy</span>
+                      </span>
+                    </label>
+                  </div>
+                </div>
+                    <button className='btn btn-primary w-full' type='submit'>
+                        Create Account
+                    </button>
+                    <div className='text-center mt-4'>
+                      <p className='text-sm'>Already have an account? {" "}
+                        <Link to="/login" className="text-primary hover:underline">Sign In</Link>
+                      </p>
+                    </div>
+              </div>
+
+            </form>
+
+          </div>
+
+      </div>
+      {/* right side */}
+      <div className=' hidden lg:flex w-full lg:w-1/2 bg-primary/10 items-center justify-center'>
+      <div className='max-w-md p-8'>
+        {/* image  */}
+          <div className='relative aspect-square max-w-sm mx-auto'>
+            <img src="/signuppic.png" alt="language connection illustration" className='w-full h-full ' />
+          </div>
+          <div className='text-center space-y-3 mt-6'>
+            <h2 className='text-xl font-semibold'> Connect With Friends Worldwide</h2>
+            <p className='opacity-70'> Practise conversation make friends and improve your language skills together</p>
+          </div>
+      </div>
+
+      </div>
+      </div>
+
+    </div>
   )
 }
 
